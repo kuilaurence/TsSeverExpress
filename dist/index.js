078564bf6d86c43724ff2aa998785f7d4513012e
KuiLaurence <kuilaurence@163.com>
2022-03-08 11:51:42 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,15 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.insertValue = void 0;
-var url_1 = __importDefault(require("url"));
-var mysql_1 = __importDefault(require("mysql"));
-var express_1 = __importDefault(require("express"));
-var body_parser_1 = __importDefault(require("body-parser"));
-var connection = mysql_1.default.createConnection({
+import url from 'url';
+import mysql from 'mysql';
+import express from 'express';
+import bodyParser from 'body-parser';
+const connection = mysql.createConnection({
     host: '192.168.3.216',
     user: 'root',
     password: 'root',
     database: 'gg',
 });
 connection.connect();
-var app = (0, express_1.default)();
+const app = express();
 app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*'); //*表示可以跨域任何域名都行 也可以填域名表示只接受某个域名
     res.header("Access-Control-Allow-Credentials", "true"); //允许跨域验证
@@ -24,19 +18,18 @@ app.all('*', function (req, res, next) {
     res.header('Content-Type', 'application/json;charset=utf-8'); //请求头中定义的类型
     next();
 });
-app.use(body_parser_1.default.urlencoded({ extended: true }));
-app.use(body_parser_1.default.json());
-var server = app.listen(8080, function () {
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const server = app.listen(8085, () => {
     console.log("就绪:", JSON.stringify(server.address()));
 });
-//http://192.168.3.216:8080/help?age=21&limit=2&offset=3
+//http://192.168.3.216:8085/help?age=21&limit=2&offset=3
 app.get('/help', function (req, res) {
-    var _a, _b;
     console.log("-----url------", req.url);
     res.status(200);
-    var parseObj = url_1.default.parse(req.url, true);
+    var parseObj = url.parse(req.url, true);
     var queryObj = parseObj.query;
-    var sql = "SELECT * FROM student WHERE  age = ".concat(queryObj.age, " LIMIT ").concat((_a = queryObj.limit) !== null && _a !== void 0 ? _a : 100, " OFFSET ").concat((_b = queryObj.offset) !== null && _b !== void 0 ? _b : 0); //limit 单次取得数量   offset跳过的数量
+    var sql = `SELECT * FROM student WHERE  age = ${queryObj.age} LIMIT ${queryObj.limit ?? 100} OFFSET ${queryObj.offset ?? 0}`; //limit 单次取得数量   offset跳过的数量
     connection.query(sql, function (error, results, fields) {
         if (error)
             console.log(error);
@@ -48,14 +41,18 @@ app.get('/help', function (req, res) {
         res.json(resJson);
     });
 });
+app.post('/test', function (req, res) {
+    console.log(req.body);
+    res.send({ massage: "post successfully!" });
+});
 /**
  * 插入一行数据
  */
-function insertValue() {
-    connection.query("INSERT INTO student(name,age)\n        VALUES ('hello',22)", function (error, results, fields) {
+export function insertValue() {
+    connection.query(`INSERT INTO student(name,age)
+        VALUES ('hello',22)`, function (error, results, fields) {
         if (error)
             throw error;
     });
 }
-exports.insertValue = insertValue;
 // connection.end();
